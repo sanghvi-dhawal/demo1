46ad3b69edb3d568a04a6e2a9dfb01533e4e4a0e
sanghvi-dhawal <dhawalsanghvi@gmail.com>
2025-10-15 19:37:21 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -7,7 +7,7 @@ def add(a, b):
 
 class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
-    self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 5)
 
     def test_add_negative_numbers(self):
         self.assertEqual(add(-1, -2), -3)
